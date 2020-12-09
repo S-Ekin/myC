35b1a33c4bda909e51421f2b83626ece14a6b60f
S3Ekin <sekin4866111@163.com>
2020-12-09 17:30:16 +0800

    一、添加新的.cpp 文件，以便好存储不同的练习文件 	1、把正在使用的。cpp文件的main函数正常使用，其他的文件的main函数换个名字，以免报错，说有多个main函数 二、自定义数据类型，，并在同一个文件；里使用数据的操作。

--- first.cpp
+++ first.cpp
@@ -1,15 +1,28 @@
+/*
+	定义数据结构，自己完成数据的操作
+*/
 #include <iostream>;
-#include "Sales.item.h";
 using namespace std;
-
-int main() {
-	Sales_item a, b;
-	std::cin >> a >> b ;
-	if (a.isbn() == b.isbn()) {
-		cout << "相同";
+#include <string>;
+int i = 23;
+struct Sales_data {
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+	double price;
+};
+int _main() {
+	Sales_data data1, data2;
+	cin >> data1.bookNo >> data1.units_sold >> data1.price;
+	data1.revenue = data1.price * data1.units_sold;
+	cin >> data2.bookNo >> data2.units_sold >> data2.price;
+	data2.revenue = data2.price * data2.units_sold;
+	if (data1.bookNo == data2.bookNo) {
+		auto total = data1.revenue + data2.revenue;
+		cout << "总价：" << total << endl;
 	}
 	else {
-		cout << "不是同一书籍";
+		cout << "书本号不相同！"<<endl;
 	}
-		return 0;
+	return 0;
 }
---
+++ second.cpp
@@ -0,0 +1,7 @@
+using namespace std;
+#include <iostream>;
+
+int main() {
+	cout << "新的文件";
+	return 0;
+}
